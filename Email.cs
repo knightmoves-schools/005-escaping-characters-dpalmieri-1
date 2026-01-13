f659ec5265dc72dfc26e6258a0902717d6b44739
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-13 18:11:47 +0000

    Email job firing.

--- Email.cs
+++ Email.cs
@@ -2,4 +2,9 @@
 public class Email
 {
     // Add your code here
+    public string introduction = "Hello Everybody! \n" +
+    "\tThis email is \"firing\" you all. \n" +
+    "That \'isn't\' a lie. \n" +
+    "Sincerely, \n" +
+    "The bosses\\companies.";
 }
