291f8c73ac34c46eb98e5337cc69d0bc7e139a89
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-15 17:43:34 +0000

    Const introduction

--- Email.cs
+++ Email.cs
@@ -2,7 +2,7 @@
 public class Email
 {
     // Add your code here
-    public const string Introduction = "Hello Everybody! \n" +
+    public const string introduction = "Hello Everybody! \n" +
     " \t This email is \" firing \" you all. \n" +
     "That \' isn't \' a lie. \n" +
     "Sincerely, \n " +
